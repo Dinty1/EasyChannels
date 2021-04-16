8adcb532743b74f91507f7bdb4200e18dec4f3a6
DB8888 <dintybmrt@gmail.com>
2021-04-16 18:19:19 +0100

    Initial Commit

---
+++ src/main/java/io/github/dinty1/easychannels/EasyChannels.java
@@ -0,0 +1,10 @@
+package io.github.dinty1.easychannels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyChannels extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
