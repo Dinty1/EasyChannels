70a9df9e969dab06be9b2f31b11a4d9da1069524
Dinty1 <dintybmrt@gmail.com>
2021-05-05 18:00:56 +0100

    Add update checker

--- src/main/java/io/github/dinty1/easychannels/EasyChannels.java
+++ src/main/java/io/github/dinty1/easychannels/EasyChannels.java
@@ -4,11 +4,9 @@ import github.scarsz.discordsrv.DiscordSRV;
 import io.github.dinty1.easychannels.command.ChannelListCommand;
 import io.github.dinty1.easychannels.command.GlobalChatCommand;
 import io.github.dinty1.easychannels.command.LeaveCommand;
-import io.github.dinty1.easychannels.listener.AsyncPlayerChatListener;
-import io.github.dinty1.easychannels.listener.DiscordGuildMessagePreProcessListener;
-import io.github.dinty1.easychannels.listener.PlayerLeaveListener;
-import io.github.dinty1.easychannels.listener.PluginEnableListener;
+import io.github.dinty1.easychannels.listener.*;
 import io.github.dinty1.easychannels.manager.ChannelManager;
+import io.github.dinty1.easychannels.object.UpdateChecker;
 import io.github.dinty1.easychannels.util.ConfigUtil;
 import lombok.Getter;
 import net.milkbowl.vault.chat.Chat;
@@ -24,6 +22,7 @@ public class EasyChannels extends JavaPlugin {
     @Getter private static ChannelManager channelManager = new ChannelManager();
     @Getter private static Chat chat;
     private static boolean discordSrvHook = false;
+    private boolean updateAvailable = false;
 
     @Override
     public void onEnable() {
@@ -43,6 +42,7 @@ public class EasyChannels extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new AsyncPlayerChatListener(), this);
         getServer().getPluginManager().registerEvents(new PluginEnableListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerLeaveListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
 
         getCommand("globalchat").setExecutor(new GlobalChatCommand());
         getCommand("channels").setExecutor(new ChannelListCommand());
@@ -54,6 +54,19 @@ public class EasyChannels extends JavaPlugin {
         // Metrics and shit
         Metrics metrics = new Metrics(this, 11106);
         metrics.addCustomChart(new SimplePie("number_of_channels", () -> String.valueOf(getChannelManager().getChannels().size())));
+
+        // Check for updates
+        getLogger().info("Checking for updates...");
+        new UpdateChecker(this, 91958).getLatestVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                getLogger().info("Up to date!");
+            } else {
+                getLogger().info("An update is available! Get it here: https://github.com/Dinty1/EasyChannels/releases");
+                this.updateAvailable = true;
+            }
+        });
+
+        getLogger().info("Enabled!");
     }
 
     @Override
@@ -94,5 +107,9 @@ public class EasyChannels extends JavaPlugin {
         if (enabled) {
             DiscordSRV.api.subscribe(new DiscordGuildMessagePreProcessListener());
         }
+    }
+
+    public boolean isUpdateAvailable() {
+        return this.updateAvailable;
     }
 }
